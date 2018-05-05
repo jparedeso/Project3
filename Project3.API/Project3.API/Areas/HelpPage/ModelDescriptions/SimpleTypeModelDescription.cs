bec9e0d10931edaaad8515f999b5815a1d02b725
Jorge Paredes <jparedeso@gmail.com>
2018-05-04 21:43:21 -0400

    Initial files

---
+++ Project3.API/Project3.API/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Project3.API.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Project3.API/Project3.API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Project3.API.WebApiApplication" Language="C#" %>
