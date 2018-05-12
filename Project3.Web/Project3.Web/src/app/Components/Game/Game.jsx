948c34b44fdd5bad690c847c49ab759ac2200faa
erin hubbard <erinhubbard1981@gmail.com>
2018-05-12 16:50:43 -0400

    began css

--- Project3.Web/Project3.Web/src/app/Components/Game/Game.jsx
+++ Project3.Web/Project3.Web/src/app/Components/Game/Game.jsx
@@ -9,10 +9,12 @@ const Game = props => {
                 return (
                     <div className='game-grid-container' key={game.id}>
                         <img src={game.imageSrc} alt={game.imageSrc} className='gameImage'/>
-                        <span className='gameName'> Title: {game.name} </span>
-                        <span className='gameValue'>Estimated Value: {game.value} </span>
-                        <span className='gameCondition'>Estimated Condition: {game.condition}</span>
+                        <span className='gameName'><strong>Title:</strong> {game.name} </span>
+                            <span className='gameValue'><strong>Estimated Value:</strong> {game.value} </span>
+                        <span className='gameCondition'><strong>Estimated Condition:</strong> {game.condition}</span>
                         <p className='gameSummary'><strong>Summary:</strong> Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto earum voluptatem facere culpa quia expedita, sunt eaque officia est nesciunt mollitia aliquam. Neque velit reiciendis blanditiis, perferendis ipsam dicta molestiae?</p>
+                        <img id="edit" className="" src="redButton.png" role="button" />
+                        <img id="save" className="" src="redButton.png" role="button" />
                     </div>
                 )
             })}
