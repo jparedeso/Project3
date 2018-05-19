099d5c232397ea6d6d3acbf29a76bc90a113fafe
fuzzy <donald.jones83@gmail.com>
2018-05-19 01:01:55 -0400

    ebay api, console logging results, need to map it into game.jsx

--- Project3.Web/Project3.Web/src/app/Components/Pages/Profile/Profile.jsx
+++ Project3.Web/Project3.Web/src/app/Components/Pages/Profile/Profile.jsx
@@ -9,11 +9,83 @@ class Profile extends Component {
         this.state = {
             loggedIn: true,
             gameCollection: [
-                { name: 'Mario', value: '$25.97', condition: 'Good', imageSrc: '/images/sm64.jpg', id: 1 },
-                { name: 'Zelda', value: '$18.75', condition: 'Alright', imageSrc: '/images/zelda64.jpg', id: 1 }
-            ]
+                { name: 'Mario', value: '$14.50', platform: 'snes', condition: 'Good', imageSrc: '/images/sm64.jpg', id: 1 },
+                { name: 'Zelda', value: '$35.25', platform: 'nes', condition: 'Alright', imageSrc: '/images/zelda64.jpg', id: 2 }
+            ],
+            value: []
         };
+
+        
+        // const nameSearch = this.state.gameCollection[0].name;
+        // const api_call = `https://cors-anywhere.herokuapp.com/https://svcs.ebay.com/services/search/FindingService/v1?RESPONSE-DATA-FORMAT=JSON&categoryId=139973&keywords=${nameSearch},nes&categoryId=139973`;
+        // console.log(api_call);
+
+        const gameSearch = this.state.gameCollection;
+        let value = this.state.value;
+
+        for (var i = 0; i < gameSearch.length; i++) {
+            const gameName = gameSearch[i].name;
+            const platformName = gameSearch[i].platform;
+            console.log(gameName);
+            console.log(platformName);
+
+
+            var settings = {
+                "async": true,
+                "crossDomain": true,
+                "url": `https://cors-anywhere.herokuapp.com/https://svcs.ebay.com/services/search/FindingService/v1?RESPONSE-DATA-FORMAT=JSON&keywords=${gameName},${platformName}&categoryId=139973`,
+                "method": "GET",
+                "headers": {
+                "X-EBAY-SOA-SERVICE-VERSION": "1.13.0",
+                "X-EBAY-SOA-OPERATION-NAME": "findCompletedItems",
+                "X-EBAY-SOA-SECURITY-APPNAME": "FuzzyJon-RetroGam-PRD-92cc9f5ed-70036e56",
+                "Cache-Control": "no-cache",
+                "Postman-Token": "2533f0d4-46b4-4ae3-90c5-bfa1c936774a"
+                }
+            }
+            
+            $.ajax(settings).done(function (response) {
+                var results = JSON.parse(response);
+                // console.log(results);
+                var items = results.findCompletedItemsResponse[0].searchResult[0].item;
+                var priceArray = [];
+        
+                for (var i = 0; i < items.length; i++) {
+                    // console.log(items[i]);
+                    var sellingState = items[i].sellingStatus[0].sellingState;
+                    var sellingPrice = items[i].sellingStatus[0].currentPrice[0].__value__;
+                    // console.log(sellingState);
+                    // var sum = 0;
+                    if (sellingState == "EndedWithSales") {
+                        // console.log("true");
+                        // console.log(sellingPrice);
+                        priceArray.push(sellingPrice);
+                        // console.log(priceArray);
+                    }
+                }
+                // console.log(priceArray);
+                
+                var sum = 0;
+        
+                for (var j = 0; j < priceArray.length; j++) {
+                    sum += parseInt( priceArray[j])
+                }
+        
+                var averagePrice = sum/priceArray.length;
+                // console.log(sum);
+                console.log(averagePrice);
+
+                value.push(averagePrice);
+        
+            });
+        }
+
+        console.log(this.state.value);
+
     }
+
+
+    
 
 
     //state = {
@@ -43,6 +115,7 @@ class Profile extends Component {
                     <div styles={styles}>
                         <Game 
                         games={this.state.gameCollection}
+                        values={this.state.value}
                         />
                     </div> :
                     <RequestLogin/>
