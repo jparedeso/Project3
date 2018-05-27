94c92f5b5253e883f384f8601ecd1ffac8ddd9ea
fuzzy <donald.jones83@gmail.com>
2018-05-27 19:47:29 -0400

    ebay converting

--- Project3.Web/Project3.Web/src/content/js/Profile.js
+++ Project3.Web/Project3.Web/src/content/js/Profile.js
@@ -99,53 +99,53 @@ const Profile = function () {
                             // the title, platform, condition, image, summary, and value will all be data.whatever from DB once api route is given and data can be tested.
                             $("#displayGames").append(
                                 `
- <div class="game-grid-container">
-    <div id="gameStats" class="row">
-      <div class="col-sm-3 gameName">
-        <strong>TITLE: </strong>Zelda: Breath of the Wild
-      </div>
-      <div class="col-sm-2 gameValue">
-        <img id="coin" src="https://media0.giphy.com/media/yCyVbqru5Ggfu/giphy.gif">
-        <strong>EST. VALUE: </strong> $41.59
-      </div>
-      <div class="col-sm-2 gameCondition dropdown-toggle" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        <strong id="conditionBtn" role="button">EST. CONDITION: </strong> EXCELLENT
-        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
-          <a class="dropdown-item" href="#">
-            <img class="lifeHeart" scr="https://pbs.twimg.com/profile_images/554699922138624000/0AopZpk4_400x400.png">
-          </a>
-          <a class="dropdown-item" href="#">4 HEARTS</a>
-          <a class="dropdown-item" href="#">3 HEARTS</a>
-          <a class="dropdown-item" href="#">2 HEARTS</a>
-          <a class="dropdown-item" href="#">1 HEART</a>
-        </div>
-      </div>
-      <div class="col-sm-2 gamePlatform">
-        <strong>PLATFORM: </strong> NES
-      </div>
-      <div class="col-sm-2 actions">
-        <img id="edit" src="https://png.icons8.com/metro/1600/edit-property.png" role="button">
-        <img id="save" src="https://cdn4.iconfinder.com/data/icons/STROKE/computer_gadgets/png/400/floppy_disk.png" role="button">
-        <img id="delete" src="https://png.icons8.com/metro/1600/delete.png" role="button">
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-sm-2 gameImage">
-        <img class='' src="" alt="" />
-      </div>
-      <br>
-      <div class="col-sm-10">
-        <p class='gameSummary'>
-          <strong>SUMMARY:</strong> Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto earum voluptatem facere culpa
-          quia expedita, sunt eaque officia est nesciunt mollitia aliquam. Neque velit reiciendis blanditiis, perferendis
-          ipsam dicta molestiae? Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto earum voluptatem facere culpa
-          quia expedita, sunt eaque officia est nesciunt mollitia aliquam. Neque velit reiciendis blanditiis, perferendis
-          ipsam dicta molestiae? Iusto earum voluptatem facere culpa quia expedita, sunt eaque officia est nesciunt mollitia
-          aliquam. Neque velit reiciendis blanditiis, perferendis ipsam dicta molestiae?
-        </p>
-      </div>
-    </div>
-</div>
+                                <div class="game-grid-container">
+                                    <div id="gameStats" class="row">
+                                    <div class="col-sm-3 gameName">
+                                        <strong>TITLE: </strong>Zelda: Breath of the Wild
+                                    </div>
+                                    <div class="col-sm-2 gameValue">
+                                        <img id="coin" src="https://media0.giphy.com/media/yCyVbqru5Ggfu/giphy.gif">
+                                        <strong>EST. VALUE: </strong> $41.59
+                                    </div>
+                                    <div class="col-sm-2 gameCondition dropdown-toggle" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        <strong id="conditionBtn" role="button">EST. CONDITION: </strong> EXCELLENT
+                                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
+                                        <a class="dropdown-item" href="#">
+                                            <img class="lifeHeart" scr="https://pbs.twimg.com/profile_images/554699922138624000/0AopZpk4_400x400.png">
+                                        </a>
+                                        <a class="dropdown-item" href="#">4 HEARTS</a>
+                                        <a class="dropdown-item" href="#">3 HEARTS</a>
+                                        <a class="dropdown-item" href="#">2 HEARTS</a>
+                                        <a class="dropdown-item" href="#">1 HEART</a>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-2 gamePlatform">
+                                        <strong>PLATFORM: </strong> NES
+                                    </div>
+                                    <div class="col-sm-2 actions">
+                                        <img id="edit" src="https://png.icons8.com/metro/1600/edit-property.png" role="button">
+                                        <img id="save" src="https://cdn4.iconfinder.com/data/icons/STROKE/computer_gadgets/png/400/floppy_disk.png" role="button">
+                                        <img id="delete" src="https://png.icons8.com/metro/1600/delete.png" role="button">
+                                    </div>
+                                    </div>
+                                    <div class="row">
+                                    <div class="col-sm-2 gameImage">
+                                        <img class='' src="" alt="" />
+                                    </div>
+                                    <br>
+                                    <div class="col-sm-10">
+                                        <p class='gameSummary'>
+                                        <strong>SUMMARY:</strong> Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto earum voluptatem facere culpa
+                                        quia expedita, sunt eaque officia est nesciunt mollitia aliquam. Neque velit reiciendis blanditiis, perferendis
+                                        ipsam dicta molestiae? Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto earum voluptatem facere culpa
+                                        quia expedita, sunt eaque officia est nesciunt mollitia aliquam. Neque velit reiciendis blanditiis, perferendis
+                                        ipsam dicta molestiae? Iusto earum voluptatem facere culpa quia expedita, sunt eaque officia est nesciunt mollitia
+                                        aliquam. Neque velit reiciendis blanditiis, perferendis ipsam dicta molestiae?
+                                        </p>
+                                    </div>
+                                    </div>
+                                </div>
                                 `
                             );
                         }
@@ -156,6 +156,63 @@ const Profile = function () {
     }
 
     function gameValue() {
+        // might exist outside function between the display games and the data loop
+        let gameName = response[i].Name;
+        let platformName = response[i].Platform;
+
+        const settings = {
+            "async"      : false,
+            "crossDomain": true,
+            "url"        : "https://cors-anywhere.herokuapp.com/https://svcs.ebay.com/services/search/FindingService/v1?RESPONSE-DATA-FORMAT=JSON&keywords=" + gameName + "," + platformName + "&categoryId=139973",
+            "method"     : "GET",
+            "headers"    : {
+                "X-EBAY-SOA-SERVICE-VERSION" : "1.13.0",
+                "X-EBAY-SOA-OPERATION-NAME"  : "findCompletedItems",
+                "X-EBAY-SOA-SECURITY-APPNAME": "FuzzyJon-RetroGam-PRD-92cc9f5ed-70036e56",
+                "Cache-Control"              : "no-cache",
+                "Postman-Token"              : "2533f0d4-46b4-4ae3-90c5-bfa1c936774a"
+            }
+        };
+
+        $.ajax(settings).done(function (response) {
+            const results = JSON.parse(response);
+            // console.log(results);
+            const items = results.findCompletedItemsResponse[0].searchResult[0].item;
+            const priceArray = [];
+
+            for (let i = 0; i < items.length; i++) {
+                // console.log(items[i]);
+                const sellingState = items[i].sellingStatus[0].sellingState;
+                const sellingPrice = items[i].sellingStatus[0].currentPrice[0].__value__;
+                // console.log(sellingState);
+                // var sum = 0;
+                if (sellingState == "EndedWithSales") {
+                    // console.log("true");
+                    // console.log(sellingPrice);
+                    priceArray.push(sellingPrice);
+                    // console.log(priceArray);
+                }
+            }
+            // console.log(priceArray);
+
+            let sum = 0;
+
+            for (let j = 0; j < priceArray.length; j++) {
+                sum += parseInt(priceArray[j]);
+            }
+
+            const average = sum / priceArray.length;
+            // console.log(sum);
+            // console.log("game name: " + gameName + "game price: " + average);
+
+            const averagePrice = average.toFixed(2);
+
+            // value.push(averagePrice);
+
+        });
+    }
+
+    function gameValue2() {
         const value = [];
 
         $.ajax({
@@ -182,8 +239,6 @@ const Profile = function () {
                             "Postman-Token"              : "2533f0d4-46b4-4ae3-90c5-bfa1c936774a"
                         }
                     };
-
-                    let that = this;
 
                     $.ajax(settings).done(function (response) {
                         const results = JSON.parse(response);
@@ -218,10 +273,8 @@ const Profile = function () {
 
                         const averagePrice = average.toFixed(2);
 
-                        value.push(averagePrice);
+                        // value.push(averagePrice);
 
-                        // that.setState({ value: value });
-                        // console.log(that.state.value, "++");
                     });
 
                 }
