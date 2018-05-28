8febd8b657f828e9308f74a44e2259a86d3e38e7
fuzzy <donald.jones83@gmail.com>
2018-05-27 21:15:11 -0400

    games added to user displaying back dynamically

--- Project3.Web/Project3.Web/src/content/js/Profile.js
+++ Project3.Web/Project3.Web/src/content/js/Profile.js
@@ -6,11 +6,6 @@ const Profile = function () {
     }
 
     function initEventHandlers() {
-        //        addGame();
-
-        // a test function to see if games can append dynamically. function is not displayGames() below add game, its below value function.
-        // this function will be be removed and not called, and instead displayGames() will be used once we have the api route.
-        //        displayGamesTest();
         searchGames();
     }
 
@@ -63,107 +58,52 @@ const Profile = function () {
                 $("#displayGames").html("");
                 for (var i = 0; i < response.length; i++) {
                     $("#displayGames").append(`
-                    <div class="gameDisplay" data-id="${response[i].gameId}">${response[i].name}</div>
+                        <div class="game-grid-container" data-id="${response[i].gameId}">
+                            <div id="gameStats" class="row">
+                                <div class="col-sm-3 gameName">
+                                    <strong>TITLE: </strong>${response[i].name}
+                                </div>
+                                <div class="col-sm-2 gameValue">
+                                    <img id="coin" src="https://media0.giphy.com/media/yCyVbqru5Ggfu/giphy.gif">
+                                    <strong>EST. VALUE: </strong> 
+                                </div>
+                                <div class="col-sm-2 gameCondition dropdown-toggle" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <strong id="conditionBtn" role="button">EST. CONDITION: </strong>
+                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
+                                    <a class="dropdown-item" href="#">
+                                        <img class="lifeHeart" scr="https://pbs.twimg.com/profile_images/554699922138624000/0AopZpk4_400x400.png">
+                                    </a>
+                                    <a class="dropdown-item" href="#">4 HEARTS</a>
+                                    <a class="dropdown-item" href="#">3 HEARTS</a>
+                                    <a class="dropdown-item" href="#">2 HEARTS</a>
+                                    <a class="dropdown-item" href="#">1 HEART</a>
+                                    </div>
+                                </div>
+                                <div class="col-sm-2 gamePlatform">
+                                    <strong>PLATFORM: </strong> 
+                                </div>
+                                <div class="col-sm-2 actions">
+                                    <img id="edit" src="https://png.icons8.com/metro/1600/edit-property.png" role="button">
+                                    <img id="save" src="https://cdn4.iconfinder.com/data/icons/STROKE/computer_gadgets/png/400/floppy_disk.png" role="button">
+                                    <img id="delete" src="https://png.icons8.com/metro/1600/delete.png" role="button">
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-2 gameImage">
+                                    <img class="" src="https:${response[i].cover}" alt="" />
+                                </div>
+                                <br>
+                                <div class="col-sm-10">
+                                    <p class='gameSummary'>
+                                    <strong>SUMMARY:</strong>${response[i].summary}
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
                     `);
                 }
             }
         });
-    }
-
-    function addGame() {
-        $("#addGameBtn").on("click", function (event) {
-            event.preventDefault();
-            const newGame = {
-                Name: $(".gameTitle").val().trim()
-            };
-
-            console.log(newGame);
-
-            // Send the POST request to create new Game.
-
-            // $.ajax("/api/game", {
-            //     type: "POST",
-            //     data: newGame
-            // }).then(
-            //     function() {
-            //         console.log("added new game");
-            //     }
-            // );
-        });
-    }
-
-    function displayGames() {
-        if ($("#displayGames")[0]) {
-            $.ajax({
-                url    : "/api/games/",
-                method : "GET",
-                success: function (data) {
-                    console.log(data);
-                    // empty to displayGames before adding new content
-                    $("#displayGames").empty();
-                    // if the data is not there, then return an error message
-                    if (!data) {
-                        $("#displayGames").append("<h2> I'm sorry, but there aren't any games yet</h2>");
-                    }
-                    else {
-                        for (let i = 0; i < data.length; i++) {
-                            // the title, platform, condition, image, summary, and value will all be data.whatever from DB once api route is given and data can be tested.
-                            $("#displayGames").append(
-                                `
-                                <div class="game-grid-container">
-                                    <div id="gameStats" class="row">
-                                    <div class="col-sm-3 gameName">
-                                        <strong>TITLE: </strong>Zelda: Breath of the Wild
-                                    </div>
-                                    <div class="col-sm-2 gameValue">
-                                        <img id="coin" src="https://media0.giphy.com/media/yCyVbqru5Ggfu/giphy.gif">
-                                        <strong>EST. VALUE: </strong> $41.59
-                                    </div>
-                                    <div class="col-sm-2 gameCondition dropdown-toggle" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                        <strong id="conditionBtn" role="button">EST. CONDITION: </strong> EXCELLENT
-                                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
-                                        <a class="dropdown-item" href="#">
-                                            <img class="lifeHeart" scr="https://pbs.twimg.com/profile_images/554699922138624000/0AopZpk4_400x400.png">
-                                        </a>
-                                        <a class="dropdown-item" href="#">4 HEARTS</a>
-                                        <a class="dropdown-item" href="#">3 HEARTS</a>
-                                        <a class="dropdown-item" href="#">2 HEARTS</a>
-                                        <a class="dropdown-item" href="#">1 HEART</a>
-                                        </div>
-                                    </div>
-                                    <div class="col-sm-2 gamePlatform">
-                                        <strong>PLATFORM: </strong> NES
-                                    </div>
-                                    <div class="col-sm-2 actions">
-                                        <img id="edit" src="https://png.icons8.com/metro/1600/edit-property.png" role="button">
-                                        <img id="save" src="https://cdn4.iconfinder.com/data/icons/STROKE/computer_gadgets/png/400/floppy_disk.png" role="button">
-                                        <img id="delete" src="https://png.icons8.com/metro/1600/delete.png" role="button">
-                                    </div>
-                                    </div>
-                                    <div class="row">
-                                    <div class="col-sm-2 gameImage">
-                                        <img class='' src="" alt="" />
-                                    </div>
-                                    <br>
-                                    <div class="col-sm-10">
-                                        <p class='gameSummary'>
-                                        <strong>SUMMARY:</strong> Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto earum voluptatem facere culpa
-                                        quia expedita, sunt eaque officia est nesciunt mollitia aliquam. Neque velit reiciendis blanditiis, perferendis
-                                        ipsam dicta molestiae? Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto earum voluptatem facere culpa
-                                        quia expedita, sunt eaque officia est nesciunt mollitia aliquam. Neque velit reiciendis blanditiis, perferendis
-                                        ipsam dicta molestiae? Iusto earum voluptatem facere culpa quia expedita, sunt eaque officia est nesciunt mollitia
-                                        aliquam. Neque velit reiciendis blanditiis, perferendis ipsam dicta molestiae?
-                                        </p>
-                                    </div>
-                                    </div>
-                                </div>
-                                `
-                            );
-                        }
-                    }
-                }
-            });
-        }
     }
 
     function gameValue() {
@@ -219,7 +159,6 @@ const Profile = function () {
             const averagePrice = average.toFixed(2);
 
             // value.push(averagePrice);
-
         });
     }
 
@@ -280,75 +219,13 @@ const Profile = function () {
 
                         const average = sum / priceArray.length;
                         // console.log(sum);
-                        // console.log("game name: " + gameName + "game price: " + average);
-
                         const averagePrice = average.toFixed(2);
-
                         // value.push(averagePrice);
-
                     });
-
                 }
             }
-
         });
     }
-
-    // this is the display game TEST function, is just displaying what used to be in the Profile.cshtml file to test dynamic html
-    // will be removed and take out the call in initEventHandlers once we have the displayGames() function working with api from DB
-    function displayGamesTest() {
-        $("#displayGames").append(
-            `
-<div class="game-grid-container">
-<div id="gameStats" class="row">
-<div class="col-sm-3 gameName">
-<strong>TITLE: </strong>Zelda: Breath of the Wild
-</div>
-<div class="col-sm-2 gameValue">
-<img id="coin" src="https://media0.giphy.com/media/yCyVbqru5Ggfu/giphy.gif">
-<strong>EST. VALUE: </strong> $41.59
-</div>
-<div class="col-sm-2 gameCondition dropdown-toggle" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-<strong id="conditionBtn" role="button">EST. CONDITION: </strong> EXCELLENT
-<div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
-<a class="dropdown-item" href="#">
-<img class="lifeHeart" scr="https://pbs.twimg.com/profile_images/554699922138624000/0AopZpk4_400x400.png">
-</a>
-<a class="dropdown-item" href="#">4 HEARTS</a>
-<a class="dropdown-item" href="#">3 HEARTS</a>
-<a class="dropdown-item" href="#">2 HEARTS</a>
-<a class="dropdown-item" href="#">1 HEART</a>
-</div>
-</div>
-<div class="col-sm-2 gamePlatform">
-<strong>PLATFORM: </strong> NES
-</div>
-<div class="col-sm-2 actions">
-<img id="edit" src="https://png.icons8.com/metro/1600/edit-property.png" role="button">
-<img id="save" src="https://cdn4.iconfinder.com/data/icons/STROKE/computer_gadgets/png/400/floppy_disk.png" role="button">
-<img id="delete" src="https://png.icons8.com/metro/1600/delete.png" role="button">
-</div>
-</div>
-<div class="row">
-<div class="col-sm-2 gameImage">
-<img class='' src="" alt="" />
-</div>
-<br>
-<div class="col-sm-10">
-<p class='gameSummary'>
-<strong>SUMMARY:</strong> Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto earum voluptatem facere culpa
-quia expedita, sunt eaque officia est nesciunt mollitia aliquam. Neque velit reiciendis blanditiis, perferendis
-ipsam dicta molestiae? Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto earum voluptatem facere culpa
-quia expedita, sunt eaque officia est nesciunt mollitia aliquam. Neque velit reiciendis blanditiis, perferendis
-ipsam dicta molestiae? Iusto earum voluptatem facere culpa quia expedita, sunt eaque officia est nesciunt mollitia
-aliquam. Neque velit reiciendis blanditiis, perferendis ipsam dicta molestiae?
-</p>
-</div>
-</div>
-</div>
-            `
-        );
-    };
 
     return {
         init: init
