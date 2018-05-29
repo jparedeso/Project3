75a7339509662892f658883b92b976c6bff3ef5d
erin hubbard <erinhubbard1981@gmail.com>
2018-05-28 20:59:59 -0400

    fixed header

--- Project3.Web/Project3.Web/src/content/js/Header.js
+++ Project3.Web/Project3.Web/src/content/js/Header.js
@@ -23,6 +23,20 @@ var Header = function() {
     });
   }
 
+  window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
   return {
     init: init
   }
