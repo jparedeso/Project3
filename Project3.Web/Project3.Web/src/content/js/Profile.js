652b9818ab42b12a131663881259cadb6db35dfd
fuzzy <donald.jones83@gmail.com>
2018-05-29 19:17:35 -0400

    base working version for profile, no value or platform dropdown

--- Project3.Web/Project3.Web/src/content/js/Profile.js
+++ Project3.Web/Project3.Web/src/content/js/Profile.js
@@ -63,8 +63,9 @@ const Profile = function () {
                         gameSelection.removeClass(gameHighlight);
                         $(this).addClass(gameHighlight);
                         console.log(id);
+                        
                         // show the platforms list that is hidden
-                        $(".platformList").show();
+                        // $(".platformList").show();
                     });
 
                     // highlight platform selection
@@ -107,15 +108,15 @@ const Profile = function () {
                 $("#displayGames").html("");
                 for (var i = 0; i < response.length; i++) {
 
-                    console.log(response[i].platforms);
+                    // console.log(response[i].platforms);
 
                     // everything commented below makes the ebay api work
-                    
+
                     // let gameName = response[i].name;
-                    // console.log(gameName);
+                    // // console.log(gameName);
 
                     // let value= [];
-                    // console.log(value);
+                    // // console.log(value);
 
                     // const settings = {
                     //     "async"      : true,
@@ -130,7 +131,7 @@ const Profile = function () {
                     //         "Postman-Token"              : "2533f0d4-46b4-4ae3-90c5-bfa1c936774a"
                     //     }
                     // };
-            
+
                     // $.ajax(settings).done(function (response) {
                     //     const results = JSON.parse(response);
                     //     // console.log(results);
@@ -163,9 +164,10 @@ const Profile = function () {
                     //     // console.log("game name: " + gameName + "game price: " + average);
             
                     //     const averagePrice = average.toFixed(2);
-                    //     console.log("game name: " + gameName + " / price: " + averagePrice);
+                    //     console.log("game: " + gameName + " / price: $" + averagePrice);
+
                     //     value.push(averagePrice);
-                    //     console.log(value);
+                    //     console.log(value); 
                         
                     // });
 
@@ -219,60 +221,64 @@ const Profile = function () {
     }
 
     function gameValue() {
-        // might exist outside function between the display games and the data loop
-        // let gameName = response[i].name;
-        // let platformName = response[i].Platform;
+        let gameName = response[i].name;
+        // console.log(gameName);
+
+        let value= [];
+        // console.log(value);
 
         const settings = {
-            "async"      : false,
-            "crossDomain": true,
-            "url"        : "https://cors-anywhere.herokuapp.com/https://svcs.ebay.com/services/search/FindingService/v1?RESPONSE-DATA-FORMAT=JSON&keywords=" + gameName + "&categoryId=139973",
-            "method"     : "GET",
-            "headers"    : {
-                "X-EBAY-SOA-SERVICE-VERSION" : "1.13.0",
-                "X-EBAY-SOA-OPERATION-NAME"  : "findCompletedItems",
-                "X-EBAY-SOA-SECURITY-APPNAME": "FuzzyJon-RetroGam-PRD-92cc9f5ed-70036e56",
-                "Cache-Control"              : "no-cache",
-                "Postman-Token"              : "2533f0d4-46b4-4ae3-90c5-bfa1c936774a"
-            }
+        "async"      : true,
+        "crossDomain": true,
+        "url"        : "https://cors-anywhere.herokuapp.com/https://svcs.ebay.com/services/search/FindingService/v1?RESPONSE-DATA-FORMAT=JSON&keywords=" + gameName + "&categoryId=139973",
+        "method"     : "GET",
+        "headers"    : {
+        "X-EBAY-SOA-SERVICE-VERSION" : "1.13.0",
+        "X-EBAY-SOA-OPERATION-NAME"  : "findCompletedItems",
+        "X-EBAY-SOA-SECURITY-APPNAME": "FuzzyJon-RetroGam-PRD-92cc9f5ed-70036e56",
+        "Cache-Control"              : "no-cache",
+        "Postman-Token"              : "2533f0d4-46b4-4ae3-90c5-bfa1c936774a"
+        }
         };
 
         $.ajax(settings).done(function (response) {
-            const results = JSON.parse(response);
-            // console.log(results);
-            const items = results.findCompletedItemsResponse[0].searchResult[0].item;
-            const priceArray = [];
+        const results = JSON.parse(response);
+        // console.log(results);
+        const items = results.findCompletedItemsResponse[0].searchResult[0].item;
+        const priceArray = [];
 
-            for (let i = 0; i < items.length; i++) {
-                // console.log(items[i]);
-                const sellingState = items[i].sellingStatus[0].sellingState;
-                const sellingPrice = items[i].sellingStatus[0].currentPrice[0].__value__;
-                // console.log(sellingState);
-                // var sum = 0;
-                if (sellingState == "EndedWithSales") {
-                    // console.log("true");
-                    // console.log(sellingPrice);
-                    priceArray.push(sellingPrice);
-                    // console.log(priceArray);
-                }
+        for (let i = 0; i < items.length; i++) {
+            // console.log(items[i]);
+            const sellingState = items[i].sellingStatus[0].sellingState;
+            const sellingPrice = items[i].sellingStatus[0].currentPrice[0].__value__;
+            // console.log(sellingState);
+            // var sum = 0;
+            if (sellingState == "EndedWithSales") {
+                // console.log("true");
+                // console.log(sellingPrice);
+                priceArray.push(sellingPrice);
+                // console.log(priceArray);
             }
-            // console.log(priceArray);
+        }
+        // console.log(priceArray);
 
-            let sum = 0;
+        let sum = 0;
 
-            for (let j = 0; j < priceArray.length; j++) {
-                sum += parseInt(priceArray[j]);
-            }
+        for (let j = 0; j < priceArray.length; j++) {
+            sum += parseInt(priceArray[j]);
+        }
 
-            const average = sum / priceArray.length;
-            // console.log(sum);
-            // console.log("game name: " + gameName + "game price: " + average);
+        const average = sum / priceArray.length;
+        // console.log(sum);
+        // console.log("game name: " + gameName + "game price: " + average);
 
-            const averagePrice = average.toFixed(2);
-            return averagePrice;
-            // value.push(averagePrice);
+        const averagePrice = average.toFixed(2);
+        console.log("game: " + gameName + " / price: $" + averagePrice);
+
+        value.push(averagePrice);
+        console.log(value); 
+
         });
-
     }
 
     return {
