4cbe91fec57d2867b74196c4907f2752d23535ea
erin hubbard <erinhubbard1981@gmail.com>
2018-05-30 14:52:17 -0400

    css details, alignment, cohesiveness

--- Project3.Web/Project3.Web/src/content/js/UserPost.js
+++ Project3.Web/Project3.Web/src/content/js/UserPost.js
@@ -25,7 +25,7 @@
             
             console.log(newPost);
             
-            $("#displayPosts").append("<div class='row'>" + "<div class='col-sm-2 userInfoCard commentComponent'>" + "<div class='card'>" + "<img class='card-img-top' src='http://images6.fanpop.com/image/photos/37500000/Paper-Mario-the-super-mario-fan-club-37561055-500-500.jpg' alt='Card image'>" + "<div class='card-body'>" + "<h4 class='card-title'>"+ "MarioFan4ever" + "</h4>" + "<p class='card-text'>" + "Member of the kingdom since: 2018" + "</p></div></div></div>" + "<div class='col-sm-9 userComment'>" + "<p>" + newPost.Content + "</div>");
+            $("#displayPosts").append("<div class='row'>" + "<div class='col-sm-2 userInfoCard commentComponent'>" + "<div class='card'>" + "<img class='card-img-top' src='http://images6.fanpop.com/image/photos/37500000/Paper-Mario-the-super-mario-fan-club-37561055-500-500.jpg' alt='Card image'>" + "<div class='card-body'>" + "<h5 class='card-title'>" + "MarioFan4ever" + "</h5>" + "<p class='card-text'>" + "Member of the kingdom since: 2018" + "</p></div></div></div>" + "<div class='col-sm-9 userComment'>" + "<p class='comment'>" + newPost.Content + "</div>");
 
         });
     }
