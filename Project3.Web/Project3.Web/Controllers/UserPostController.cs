2f6c08cb7f0fb96cea28992142f7cfed44493072
fuzzy <donald.jones83@gmail.com>
2018-05-29 21:55:24 -0400

    static condition and value, stuck on userpost route

---
+++ Project3.Web/Project3.Web/Controllers/UserPostController.cs
@@ -0,0 +1,16 @@
+// using System;
+// using System.Collections.Generic;
+// using Microsoft.AspNetCore.Mvc;
+// using Project3.Web.Models;
+
+
+// namespace Project3.Web.Controllers
+// {
+//     public class UserPostController : Controller
+//     {
+//         public ActionResult UserPost()
+//         {
+//             return View();
+//         }
+//     }
+// }
