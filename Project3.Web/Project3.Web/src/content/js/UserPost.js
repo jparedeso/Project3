382c981c43b778f6fd7c51c367675ba5ffa293ff
erin hubbard <erinhubbard1981@gmail.com>
2018-05-30 01:00:43 -0400

    cleaned up css again

--- Project3.Web/Project3.Web/src/content/js/UserPost.js
+++ Project3.Web/Project3.Web/src/content/js/UserPost.js
@@ -1,1 +1,40 @@
-// Just a comment to have something in file so files push
+
+
+  $(document).ready(function () {
+    $("addPost").click(function () {
+      $("commentModal").modal();
+    });
+  });
+  
+  var post = function() {
+    function init() {
+        initEventHandlers();
+    }
+
+    function initEventHandlers() {
+        submitPost();
+    }
+    
+    function submitPost() {
+        $("#submitPost").on("click", function(event) {
+            event.preventDefault();
+
+            var newPost = {
+                Content: $("#newPost").val().trim()
+            };
+            
+            console.log(newPost);
+            
+            $("#displayPosts").append("<div class='row'>" + "<div class='col-sm-2 userInfoCard commentComponent'>" + "<div class='card'>" + "<img class='card-img-top' src='http://images6.fanpop.com/image/photos/37500000/Paper-Mario-the-super-mario-fan-club-37561055-500-500.jpg' alt='Card image'>" + "<div class='card-body'>" + "<h4 class='card-title'>"+ "MarioFan4ever" + "</h4>" + "<p class='card-text'>" + "Member of the kingdom since: 2018" + "</p></div></div></div>" + "<div class='col-sm-9 userComment'>" + "<p>" + newPost.Content + "</div>");
+
+        });
+    }
+
+    return {
+        init: init
+    }
+  }();
+
+  $(function() {
+      post.init();
+  });
